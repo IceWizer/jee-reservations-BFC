027db9f3906454b7e2eca5a32e57d3ffb24e2bea
Vincent DIRIBARNE <vdiribarnepro@gmail.com>
2024-10-03 09:25:37 +0200

    Init project

---
+++ src/test/java/com/bfv/reservation/ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfv.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
